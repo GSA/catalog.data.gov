8f622b6c8d1ed0298a6893cc76dcd299756e1516
Andres Vazquez <andres@data99.com.ar>
2020-05-19 09:55:28 -0300

    Detect errors before read response

--- tools/harvest_source_import/remote_ckan/lib.py
+++ tools/harvest_source_import/remote_ckan/lib.py
@@ -117,6 +117,16 @@ class RemoteCKAN:
             error = 'ERROR creating harvest source: {} [{}]'.format(e, ckan_package)
             return False, 0, error
 
+        if req.status_code == 409:
+            return False, req.status_code, 'Harvest source already exists'
+
+        if req.status_code >= 400:
+            error = ('ERROR creating harvest source: {}'
+                     '\n\t Status code: {}'
+                     '\n\t content:{}'.format(ckan_package, req.status_code, content))
+            logger.error(error)
+            return False, req.status_code, error
+
         content = req.content
         try:
             json_content = json.loads(content)
@@ -124,16 +134,6 @@ class RemoteCKAN:
             error = 'ERROR parsing JSON data: {} [{}]'.format(content, e)
             logger.error(error)
             return False, 0, error
-
-        if req.status_code >= 409:
-            return False, req.status_code, 'Harvest source already exists'
-            
-        if req.status_code >= 400:
-            error = ('ERROR creating harvest source: {}'
-                     '\n\t Status code: {}'
-                     '\n\t content:{}'.format(ckan_package, req.status_code, content))
-            logger.error(error)
-            return False, req.status_code, error
 
         if not json_content['success']:
             error = 'API response failed: {}'.format(json_content.get('error', None))
