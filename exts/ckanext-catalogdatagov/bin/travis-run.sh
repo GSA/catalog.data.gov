cf16550cad5bd0bcc0056a269e8a8be667094f01
Andres Vazquez <andres@data99.com.ar>
2020-05-20 08:04:40 -0300

    Add new extension to start adding customizations

---
+++ exts/ckanext-catalogdatagov/bin/travis-run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
+
+nosetests --ckan \
+          --nologcapture \
+          --with-pylons=subdir/test.ini \
+          --with-coverage \
+          --cover-package=ckanext.catalogdatagov \
+          --cover-inclusive \
+          --cover-erase \
+          --cover-tests
---
+++ exts/ckanext-catalogdatagov/ckanext/catalogdatagov/tests/test_plugin.py
@@ -0,0 +1,5 @@
+"""Tests for plugin.py."""
+import ckanext.catalogdatagov.plugin as plugin
+
+def test_plugin():
+    pass
