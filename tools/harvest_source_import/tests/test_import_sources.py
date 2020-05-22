e0844459902212e1caa67fc47a647149c3872f09
Andres Vazquez <andres@data99.com.ar>
2020-05-22 15:26:45 -0300

    Fix tests

--- tools/harvest_source_import/tests/test_import_sources.py
+++ tools/harvest_source_import/tests/test_import_sources.py
@@ -5,37 +5,31 @@ from remote_ckan.lib import RemoteCKAN
 @pytest.mark.vcr()
 def test_load_from_url():
     """ Test with some previous harvester already saved
-        Use a pytest cassette so real requests are not required. """
+        Use a pytest cassette so real requests are not required. 
+        We import 3 harvest sources (so they already exists) 
+        and then run this test with 6 sources. """
 
     ckan = RemoteCKAN(url='https://catalog.data.gov')
     ckan.set_destination(ckan_url='http://ckan:5000',
-                         ckan_api_key='2feae71c-0aed-453f-85f4-1bb7bfdb6e63')
-
-    harvest_sources = []
-    ok = 0
-    failed = 0
-    already_exists = 0
+                         ckan_api_key='77d957e1-510c-414d-b4dd-d4317cd253ae')
 
     print('Getting harvest sources ...')
-    for hs in ckan.list_harvest_sources(source_type='datajson'):
+    for hs in ckan.list_harvest_sources(source_type='datajson', limit=6):
 
-        harvest_sources.append(hs)
-        print('Getting {}'.format(hs['name']))
-        # save to destination CKAN    
-        created, status_code, error = ckan.create_harvest_source(data=hs)
-        if created: 
-            ok += 1
-        elif error == 'Already exists':
-            already_exists += 1
-        else:
-            failed += 1
-        
-        # limit
-        if ok + already_exists + failed == 20:
-            break
+        ckan.create_harvest_source(data=hs)
+        assert 'created' in ckan.harvest_sources[hs['name']].keys()
+        assert 'updated' in ckan.harvest_sources[hs['name']].keys()
+        assert 'error' in ckan.harvest_sources[hs['name']].keys()
 
-    print('Finished: {} harvest sources. {} Added, {} already exists, {} failed'.format(len(harvest_sources), ok, already_exists, failed))
-    
-    assert ok == 10
-    assert failed == 0
-    assert already_exists == 10
+    created = len([k for k, v in ckan.harvest_sources.items() if v['created'] ])
+    updated = len([k for k, v in ckan.harvest_sources.items() if v['updated'] ])
+    errors = len([k for k, v in ckan.harvest_sources.items() if v['error'] ])
+    total = created + updated + errors
+
+    print('Finished: {} harvest sources. {} Added, {} already exists, {} failed'.format(total, created, updated, errors))
+
+    assert total == len(ckan.harvest_sources)
+    assert created == 3
+    assert updated == 3
+    assert errors == 0
+
