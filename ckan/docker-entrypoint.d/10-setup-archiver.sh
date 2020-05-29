5c1cf29700ca189a9ed63335f81f4d080d137621
Andres Vazquez <andres@data99.com.ar>
2020-05-29 12:13:13 -0300

    Fix config path

--- ckan/docker-entrypoint.d/10-setup-archiver.sh
+++ ckan/docker-entrypoint.d/10-setup-archiver.sh
@@ -1,5 +1,5 @@
 echo "Setup Report database table"
-paster --plugin=ckanext-report report initdb --config=mysite.ini
+paster --plugin=ckanext-report report initdb --config=$CKAN_INI
 
 echo "Setup Archiver database table"
 paster --plugin=ckanext-archiver archiver init --config=$CKAN_INI
