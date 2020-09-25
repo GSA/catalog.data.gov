8a99fd36bd853e9a0c714f3f952d5292e9962a93
Aaron D Borden <adborden@a14n.net>
2020-09-24 17:33:15 -0700

    [tools] add errored column to report

--- tools/harvest_source_import/list_harvest_sources.py
+++ tools/harvest_source_import/list_harvest_sources.py
@@ -17,7 +17,7 @@ ckan = RemoteCKAN(url=args.origin_url, user_agent=args.user_agent)
 
 csvfile = open(f'report-{args.source_type}.csv', 'w')
 fieldnames = ['title', 'name', 'type', 'url', 'frequency',
-              'job_count', 'total_datasets', 'last_job_created',
+              'job_count', 'total_datasets', 'last_job_errored', 'last_job_created',
               'last_job_finished', 'last_job_status']
 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -36,7 +36,8 @@ for hs in ckan.list_harvest_sources(source_type=args.source_type):
     last_job = status.get('last_job', {})
     if last_job is None:
         last_job = {}
-        
+    stats = last_job.get('stats', {})
+
     row = {'title': hs.get('title', 'undefined'),
            'name': hs['name'],
            'type': hs.get('source_type', 'undefined'),
@@ -46,11 +47,12 @@ for hs in ckan.list_harvest_sources(source_type=args.source_type):
            'total_datasets': status.get('total_datasets', 'undefined'),
            'last_job_created': last_job.get('created', 'undefined'),
            'last_job_finished': last_job.get('finished', 'undefined'),
-           'last_job_status': last_job.get('status', 'undefined')
+           'last_job_status': last_job.get('status', 'undefined'),
+           'last_job_errored': stats.get('errored', 0),
         }
     writer.writerow(row)
 
-    
+
 print('Finished: {} harvest sources with {} error(s)'.format(len(harvest_sources), len(ckan.errors)))
 if len(ckan.errors) > 0:
     print('*******\nWITH ERRORS\n*******')
