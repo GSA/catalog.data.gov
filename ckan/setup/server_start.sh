0bf04373eaa9c7e1478bedf853417189490d9cce
jbrown-xentity <jbrown@xentity.com>
2020-10-21 08:28:37 -0600

    Add env option

--- ckan/setup/server_start.sh
+++ ckan/setup/server_start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+IF test -f ".env"; then
+    set -o allexport; source .env; set +o allexport
+fi
+
 # Run web application
-newrelic-admin run-program gunicorn --worker-class gevent --paste $CKAN_INI "$@"
+$CKAN_PY_ENV/newrelic-admin run-program $CKAN_PY_ENV/gunicorn --worker-class gevent --paste $CKAN_INI "$@"
