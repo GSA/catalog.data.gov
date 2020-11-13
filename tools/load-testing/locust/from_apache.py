6ee0af7cbc8afb99271c2cf5be48742a2496db91
Andres Vazquez <andres@data99.com.ar>
2020-11-13 12:02:24 -0300

    Get lang prefixed URLs

--- tools/load-testing/locust/from_apache.py
+++ tools/load-testing/locust/from_apache.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import random
 from locust import HttpUser, task, between
 
@@ -14,6 +15,14 @@ generator = generate_url()
 class AnonUser(HttpUser):
     wait_time = between(0.1, 0.2)
 
+    def get_name(self, next_url):
+        if next_url.startswith('/organization'): return 'org'
+        elif next_url.startswith('/group'): return 'group'
+        elif next_url.startswith('/dataset'):return 'dataset'
+        elif next_url.startswith('/harvest'): return 'harvest'
+        elif next_url.startswith('/api'): return 'api'
+        else: return None
+
     @task
     def from_apache_logs(self):
         try:
@@ -23,16 +32,23 @@ class AnonUser(HttpUser):
             self.environment.runner.quit()
         
         # to skip thousand of lines we need to group this call using names
-        name = 'others'
-        if next_url.startswith('/organization'):
-            name ='org'
-        elif next_url.startswith('/group'):
-            name ='group'
-        elif next_url.startswith('/dataset'):
-            name ='dataset'
-        elif next_url.startswith('/harvest'):
-            name ='harvest'
-        elif next_url.startswith('/api'):
-            name ='api'
+        name = self.get_name(next_url)
+        if name is None:
+            # look for language URLs
+            URL_REGEX = r'\/(?P<lang>([A-Za-z_-]+))\/(?P<main>.*)'
+            compiled = re.compile(URL_REGEX)
+        
+            match = compiled.match(next_url)
+            if match is None:
+                print('Not match for \n\t[{}]'.format(next_url))
+            else:
+                data = match.groupdict()
+                name = self.get_name("/" + data['main'])
+                if name is not None:
+                    name = 'LANG_' + name
+
+        if name is None:        
+            name = 'others'
+            print('{} not recognized'.format(next_url))
             
         self.client.get(next_url, name=name)
