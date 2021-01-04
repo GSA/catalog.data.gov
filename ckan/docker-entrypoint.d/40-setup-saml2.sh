0a906a569a3ba5b1d0395d6165571db53c07f5cd
Andres Vazquez <andres@data99.com.ar>
2021-01-04 11:16:02 -0300

    Connect to upsteram repo

--- ckan/docker-entrypoint.d/40-setup-saml2.sh
+++
@@ -1,6 +0,0 @@
-if [[ "$CKAN__PLUGINS" == *"saml2"* ]]; then
-    echo "Setup ckanext-saml2"
-    paster --plugin=ckanext-saml2 saml2 create --config=$CKAN_INI
-else
-    echo "Skipping saml2 initialization"
-fi
