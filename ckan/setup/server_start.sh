cdc7b71f690a35df7fa7958501cec77f635cb24f
Fuhu Xia <fxia@reisystems.com>
2021-05-14 16:35:40 -0400

    add missing $DIR

--- ckan/setup/server_start.sh
+++ ckan/setup/server_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
 # Run web application
 exec newrelic-admin run-program gunicorn -c "$DIR/gunicorn.conf.py" --worker-class gevent --paste $CKAN_INI "$@"
