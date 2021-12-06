0dfe29b1565651f384c397b5647ed27598a970cc
robert-bryson <robert.bryson@gsa.gov>
2021-12-06 12:32:03 -0800

    Working migrate-solrcloud-schema.sh

--- solr/migrate-solrcloud-schema.sh
+++ solr/migrate-solrcloud-schema.sh
@@ -1,36 +1,43 @@
-#!/bin/bash 
+#!/bin/bash
+set -e
+
+COLLECTION_NAME=${1:-ckan}
 
 # https://gist.github.com/adborden/4b2ecc9d679556ac436b0616d9ddd3b2
 
-SOLR_URL='https://default-solr-6bb1c3054ccac59d-solrcloud.7f36d1b4-dad4-4d1b-90c9-f7014c20d9c9.ssb-dev.data.gov/solr'
+# error out if environment variables not set
+if [[ -z "$CKAN_SOLR_URL" ]]; then
+    echo "Must provide CKAN_SOLR_URL in environment" 1>&2
+    exit 1
+elif [[ -z "$CKAN_SOLR_USER" ]]; then
+    echo "Must provide CKAN_SOLR_USER in environment" 1>&2
+    exit 1
+elif [[ -z "$CKAN_SOLR_PASSWORD" ]]; then
+    echo "Must provide CKAN_SOLR_PASSWORD in environment" 1>&2
+    exit 1
+fi
 
 # Check if the solr core exists.
-if ! (curl --get --fail --silent http://solr:8983/solr/admin/cores \
-    --data-urlencode action=status \
-    --data-urlencode core=inventory | grep -q segmentsFileSizeInBytes); then
-
-    # Create the solr core
-    curl -v --get --fail --silent http://solr:8983/solr/admin/cores \
-    --data-urlencode action=create \
-    --data-urlencode name=inventory \
-    --data-urlencode configSet=ckan2_8
+# TODO: add curl to follow redirect
+if ! (curl --get --fail --location-trusted --silent --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
+    $CKAN_SOLR_URL/solr/admin/collections \
+    --data-urlencode action=list \
+    --data-urlencode wt=json | grep -q $COLLECTION_NAME); then
 
     # Zip solr configSet
     zip ckan_2.9_solr_config.zip \
-        currency.xml  elevate.xml  protwords.txt  schema.xml  solrconfig.xml stopwords.txt  synonyms.txt
+        currency.xml  elevate.xml  protwords.txt  schema.xml  solrconfig.xml stopwords.txt  synonyms.txt \
+        > /dev/null
 
-    # Upload configSet
-    curl -v --user $SOLR_CLOUD_CREDS \
-        "$SOLR_URL/admin/configs?action=upload&name=ckan_v1" \
-        --data-binary @ckan_2.9_solr_config.zip --header 'Content-Type:application/octet-stream'
+    echo "Uploading config set..."
+    curl --fail --silent --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
+        "$CKAN_SOLR_URL/solr/admin/configs?action=upload&name=$COLLECTION_NAME" \
+        --data-binary @ckan_2.9_solr_config.zip --header 'Content-Type:application/octet-stream' \
+        > /dev/null
 
-    # Create solr collection
-    curl -v --user $SOLR_CLOUD_CREDS \
-        "$SOLR_URL/admin/collections?action=create&name=ckan&collection.configName=ckan_v1&numShards=1" \
-        -X POST
-
-    # Reload the core
-    curl -v --get --fail --silent http://solr:8983/solr/admin/cores \
-    --data-urlencode action=reload \
-    --data-urlencode core=inventory
+    echo "Creating solr connection..."
+    curl --fail --silent --location-trusted --user $CKAN_SOLR_USER:$CKAN_SOLR_PASSWORD \
+        "$CKAN_SOLR_URL/solr/admin/collections?action=create&name=$COLLECTION_NAME&collection.configName=$COLLECTION_NAME&numShards=1" \
+        -X POST \
+        > /dev/null
 fi
