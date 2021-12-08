256f7a0e9ee1a11e05bfbfe8182b07107d4044c1
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-12-08 13:15:28 -0800

    Better messaging about the state of services

--- create-cloudgov-services.sh
+++ create-cloudgov-services.sh
@@ -24,6 +24,13 @@ wait
 
 # Check that all the services are in a healthy state. (The OSBAPI spec says that
 # the "last operation" should include "succeeded".)
-cf service "${app_name}-db"         | grep -q "status:.*succeeded$"
-cf service "${app_name}-redis"      | grep -q "status:.*succeeded$"
-cf service "${app_name}-solr"       | grep -q "status:.*succeeded$"
+success=0;
+for service in "${app_name}-db" "${app_name}-redis" "${app_name}-solr"" 
+do
+    status=$(cf service "$service" | grep 'status:\s*.\+$' )
+    echo "$service $status"
+    if ! echo "$status" | grep -q 'succeeded' ; then
+        success=1;
+    fi
+done
+exit $success
