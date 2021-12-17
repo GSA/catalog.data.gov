72ec967c38716506b3f41414a300131563f5c22c
jbrown-xentity <jbrown@xentity.com>
2021-12-17 10:19:34 -0700

    gunicorn path wasn't found
    Use previous $DIR value

--- ckan/setup/server_start.sh
+++ ckan/setup/server_start.sh
@@ -5,4 +5,4 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # Run web application
 # exec newrelic-admin run-program gunicorn -c "$DIR/gunicorn.conf.py" --worker-class gevent --paste $CKAN_INI "$@"
-exec newrelic-admin run-program gunicorn "wsgi:application" --config "./gunicorn.conf.py" -b "0.0.0.0:$PORT"
+exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT"
