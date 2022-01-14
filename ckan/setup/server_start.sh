222a9dfa2dbd9e1f7a02e006c6907107f071a382
Nicholas Kumia <nicholas.kumia@gsa.gov>
2022-01-14 10:02:26 -0500

    new: add gevent and update worker count
    Different configurations of gunicorn parameters have been tested; this configuration provides the lowest known request queueing time at this time

--- ckan/setup/server_start.sh
+++ ckan/setup/server_start.sh
@@ -4,5 +4,8 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 # sleep 1000
 
 # Run web application
-# exec newrelic-admin run-program gunicorn -c "$DIR/gunicorn.conf.py" --worker-class gevent --paste $CKAN_INI "$@"
-exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR  --timeout 120 --workers 2
+# Each worker requires ~100MB of RAM
+#   - 2 workers needs total 350MB RAM
+#   - 4 Morkers needs total 550MB RAM
+# Threads RAM requirement unknown at this time
+exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR  --timeout 120 --worker-class gevent --workers 4 --threads 4
