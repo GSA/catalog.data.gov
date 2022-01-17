f8d24d6dbb21522e32df13fd000ad1f6c51ee96d
Nicholas Kumia <nicholas.kumia@gsa.gov>
2022-01-17 11:02:05 -0500

    new: add nginx conf
    Force gunicorn to accept all ips forwards from proxy app; add gunicorn nginx.conf file; add default error page if ckan app is not available

--- ckan/setup/server_start.sh
+++ ckan/setup/server_start.sh
@@ -5,4 +5,4 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # Run web application
 # exec newrelic-admin run-program gunicorn -c "$DIR/gunicorn.conf.py" --worker-class gevent --paste $CKAN_INI "$@"
-exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR  --timeout 120 --workers 2
+exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR  --timeout 120 --workers 2 --forwarded-allow-ips='*'
