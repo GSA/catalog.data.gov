86dd7b1d056e2fd4d37b91a2d1230b5a86aff84a
Fuhu Xia <fxia@reisystems.com>
2022-02-14 12:24:11 -0500

    add cypress test for map, location and redirect

--- e2e/cypress/integration/main_page.spec.js
+++ e2e/cypress/integration/main_page.spec.js
@@ -1,5 +1,13 @@
 describe('Main Page', () => {
     
+    it('Redirects from / to /dataset', () => {
+        cy.visit('/')
+        cy.url().should(
+            'be.equal',
+            `${Cypress.config("baseUrl")}/dataset`
+        )
+    });
+
     it('Load main page with configuration', () => {
         cy.visit('/dataset')
         cy.contains('Data Catalog')
