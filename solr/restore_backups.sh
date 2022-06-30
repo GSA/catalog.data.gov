5266099ce253f0b18366d26dd5496251754f7180
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-06-30 13:53:06 -0400

    refactor: only need two scripts, not three

--- solr/restore_backups.sh
+++ solr/restore_backups.sh
@@ -4,9 +4,6 @@
 good_backup=`ls | grep aws-backup-restore | tail -1`
 mv $good_backup/ckan /var/solr/data/ckan
 
-# Remove any unused data
-rm -rf /var/solr/data/aws-backup-restore*
-
 # Download the main setup
 wget -O common_setup.sh https://raw.githubusercontent.com/GSA/catalog.data.gov/main/solr/solr_setup.sh
 chmod 755 common_setup.sh
