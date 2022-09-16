de3788c2743ab0f89f2cf8e1c6644c9550ca11b8
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-16 16:03:25 -0400

    update: add cypress test for extension inclusions

--- e2e/cypress/integration/ckan_extensions.spec.js
+++ e2e/cypress/integration/ckan_extensions.spec.js
@@ -35,10 +35,10 @@ describe('CKAN Extensions', () => {
             expect(installed_extensions).to.include('dcat_json_interface');
             expect(installed_extensions).to.include('structured_data');
             expect(installed_extensions).to.include('datagovcatalog');
+            expect(installed_extensions).to.include('report');
+            expect(installed_extensions).to.include('archiver');
 
             // TODO: The following extensions should be verified to be installed:
-            // expect(installed_extensions).to.include('report');
-            // expect(installed_extensions).to.include('archiver');
             // expect(installed_extensions).to.include('qa');
         });
     });
