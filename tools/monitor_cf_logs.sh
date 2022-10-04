4519ccd1b76fd22d7917e2a13cacff14d1e74d0c
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-10-04 09:38:24 -0400

    fix: censor logs further
    Only print out logs for a specified extension

--- tools/monitor_cf_logs.sh
+++ tools/monitor_cf_logs.sh
@@ -2,11 +2,12 @@
 
 # Custom script to look at logs from run task...
 
-# Input any app name
+# Input any app name, any task name and any log key
 app_to_monitor=$1
 task_to_monitor=$2
+logs_to_print=$3
 
-cf logs "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\]" &
+cf logs "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\]" | grep "\[ckanext\.$logs_to_print\]" &
 BACKGROUND_PID=$!
 
 while ! ( cf logs --recent "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\] OUT Exit status 0" )
