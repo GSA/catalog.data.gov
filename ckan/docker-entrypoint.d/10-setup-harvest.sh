bccb04d8d4be76d36cb6db693169eec3abf3fbd7
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-10-24 16:24:03 -0400

    new: run harvester check every 10 secs

--- ckan/docker-entrypoint.d/10-setup-harvest.sh
+++ ckan/docker-entrypoint.d/10-setup-harvest.sh
@@ -8,9 +8,13 @@ ckan harvester fetch-consumer &
 ckan harvester fetch-consumer &
 ckan harvester gather-consumer &
 
-echo "check harvest job completion every minute"
-# Don't know why, but it needs to be here three times to work...
-echo "*/1 * * * * ckan harvester run &> /tmp/harvester_run.log" | crontab -
-echo "*/1 * * * * ckan harvester run &> /tmp/harvester_run.log" | crontab -
-echo "*/1 * * * * ckan harvester run &> /tmp/harvester_run.log" | crontab -
-/etc/init.d/cron start
+echo "check harvest job completion every 10 secs"
+check_harvester () {
+  while true
+  do
+    ckan harvester run &> /tmp/harvester_run.log
+    sleep 10
+  done
+}
+
+check_harvester &
