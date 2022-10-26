221df628dc0592297fcbdbeb0c4cc5650b283bc8
Fuhu Xia <fxia@reisystems.com>
2022-10-25 22:27:27 -0400

    get cf log to work

--- tools/monitor_cf_logs.sh
+++ tools/monitor_cf_logs.sh
@@ -3,27 +3,16 @@
 # Custom script to look at logs from run task...
 
 # Input any app name, any task name and any log key
+# log key helps to prevent printing sensitive info such as redis password
 app_to_monitor=$1
 task_to_monitor=$2
 logs_to_print=$3
 
-cf logs "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\]" | grep "$logs_to_print\]" &
-BACKGROUND_PID=$!
-
-while ! ( cf logs --recent "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\] OUT Exit status" )
-do 
-  sleep 2
-done
-
-if cf logs --recent "$app_to_monitor" | grep "\[APP/TASK/$task_to_monitor/0\] OUT Exit status 0"
-then
-  exit_code=0
-else
-  exit_code=1
-fi
-
-kill -9 $BACKGROUND_PID
-kill -9 $((BACKGROUND_PID-1))
-kill -9 $((BACKGROUND_PID-2))
-
-exit $exit_code
+while read line ; do
+  echo $line | grep --line-buffered "$logs_to_print\]"
+  if echo $line | grep "OUT Exit status 0"; then
+    exit 0
+  elif echo $line | grep "OUT Exit status"; then
+    exit 1
+  fi
+done < <(cf logs "$app_to_monitor" | grep  --line-buffered "\[APP/TASK/$task_to_monitor/0\]")
