7db67250ce1da43e922929a928b627fea3cd4a31
robert-bryson <robert.bryson@gsa.gov>
2023-03-10 18:43:24 +0000

    trying with ngnix

--- vendor-nginx-modules.sh
+++ vendor-nginx-modules.sh
@@ -4,35 +4,41 @@
 # specified in vendor-requirements.txt into the "vendor" directory. The actual process runs inside a Docker
 # container; Docker is the only local prerequisite.
 
-# Get the latest version of the cflinuxfs3 image
-if [[ "$1" == "build" ]]; then
-  docker build -t nginx-vendor proxy/
-fi
+git clone https://github.com/openresty/redis2-nginx-module.git
+wget 'http://nginx.org/download/nginx-1.23.3.tar.gz'
 
 # The bind mount here enables us to write back to the host filesystem
 docker run \
     --mount type=bind,source="$(pwd)",target=/home/vcap/app \
     --tmpfs /home/vcap/app/src \
     --name cf_bash \
-    --rm -i nginx-vendor:latest  /bin/bash \
+    --rm \
+    --pull always \
+    --interactive \
+    nginx:1.23.3 /bin/bash \
     -eu \
     <<EOF
 
 # Go where the app files are
-cd ~vcap/app
+cd /home/vcap/app
 
-# As the VCAP user, cache .whls based on the frozen requirements for vendoring
-git clone https://github.com/openresty/redis2-nginx-module.git
-wget 'http://nginx.org/download/nginx-1.23.3.tar.gz'
 tar -xzvf nginx-1.23.3.tar.gz
 cd nginx-1.23.3/
 ./configure --prefix=/opt/nginx --add-dynamic-module=/home/vcap/app/redis2-nginx-module
 make -j2
 make install
+
+# copy module back to host
 cd ../
 mkdir proxy/modules
 cp /opt/nginx/modules/ngx_http_redis2_module.so proxy/modules/ngx_http_redis2_module.so
+
+# cleanup
 rm -rf nginx-1.*
 rm -rf redis2-nginx-module
 
 EOF
+
+# cleanup
+rm -rf redis2-nginx-module/
+rm -rf nginx-1.*
