5977c63f4b34dc3657c82d01e5000a3768771d44
Nicholas Kumia <nicholas.kumia@reisystems.com>
2023-03-17 15:01:31 -0400

    new: get module to be binary compatible
    - When compiling the module, the same flags need to be set as they were when the nginx binary was compiled.
    - The 'echo_request_body' argument/variable needs separate module 'ngx_http_echo_module'.
    - The default flags also need the ssl dependencies.
    - No need to track *.so files in repo

--- vendor-nginx-modules.sh
+++ vendor-nginx-modules.sh
@@ -5,6 +5,7 @@
 # container; Docker is the only local prerequisite.
 
 git clone https://github.com/openresty/redis2-nginx-module.git
+git clone https://github.com/openresty/echo-nginx-module.git
 wget 'http://nginx.org/download/nginx-1.21.3.tar.gz'
 
 # The bind mount here enables us to write back to the host filesystem
@@ -19,24 +20,26 @@ docker run \
     -eu \
     <<EOF
 apt update
-apt install gcc libpcre3-dev zlib1g-dev make -y
+apt install gcc libpcre3-dev zlib1g-dev make libssl-dev -y
 
 # Go where the app files are
 cd /home/vcap/app
 
 tar -xzvf nginx-1.21.3.tar.gz
 cd nginx-1.21.3/
-./configure --prefix=/opt/nginx --add-dynamic-module=/home/vcap/app/redis2-nginx-module --with-compat
+./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-cc-opt='-g -O2 -fdebug-prefix-map=/data/builder/debuild/nginx-1.21.3/debian/debuild-base/nginx-1.21.3=. -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC' --with-ld-opt='-Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie'  --add-dynamic-module=/home/vcap/app/redis2-nginx-module --add-dynamic-module=/home/vcap/app/echo-nginx-module
 make -j2
 make install
 
 # copy module back to host
 cd ../
-mkdir proxy/modules
-cp /opt/nginx/modules/ngx_http_redis2_module.so proxy/modules/ngx_http_redis2_module.so
+mkdir -p proxy/
+cp /etc/nginx/modules/ngx_http_redis2_module.so proxy/ngx_http_redis2_module.so
+cp /etc/nginx/modules/ngx_http_echo_module.so proxy/ngx_http_echo_module.so
 
 # cleanup
 rm -rf nginx-1.*
 rm -rf redis2-nginx-module
+rm -rf redis2-echo-module
 
 EOF
