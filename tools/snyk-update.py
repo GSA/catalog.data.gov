6297907cfc47784f4151cd00e581ff9b77ec8b4c
Nicholas Kumia <nicholas.kumia@reisystems.com>
2023-04-03 14:55:20 -0400

    revert: only update if explicit reference did not work

--- tools/snyk-update.py
+++ tools/snyk-update.py
@@ -10,22 +10,15 @@ scan_file.close()
 # Make changes to fixable results
 remediations = scan['remediation']['pin']
 for k, v in remediations.items():
-    update = False
     package, old_version = k.split('@')
     new_version = v['upgradeTo'].split('@')[1]
     print(package, old_version, new_version)
 
-    # Check to see if package was explicitly included
-    # If it was not, then we only care about updating
-    # requirements.txt, not requirements.in
-    # 'make update-dependencies' will update requirements.txt
-    with open('ckan/requirements.in', 'r') as source:
-        all_requirements = source.readlines()
-    if package in ', '.join(all_requirements):
-        update = True
+    # TODO: Handle case when vulnerable package isn't explicitly in requirements.in
 
-    if update:
-        os.system('sed -i "/^%s\\(=\\|>\\|$\\)/Id" ckan/requirements.in' % (package))
-        os.system("echo '%s' >> ckan/requirements.in" % (package + ">=" + new_version))
+    # Remove old version
+    os.system('sed -i "/^%s\\(=\\|>\\|$\\)/Id" ckan/requirements.in' % (package))
+    # Add new version
+    os.system("echo '%s' >> ckan/requirements.in" % (package + ">=" + new_version))
 
 # TODO: Handle unfixable results
