3a4833774c5d865d3efc5742b10df1a26d181a9f
Nicholas Kumia <nicholas.kumia@reisystems.com>
2023-04-03 14:35:34 -0400

    fix: reset update to False for the next package
    Otherwise, the first package will set update for every package

--- tools/snyk-update.py
+++ tools/snyk-update.py
@@ -7,25 +7,25 @@ scan_file = open('scan.json')
 scan = json.load(scan_file)
 scan_file.close()
 
-update = False
-
 # Make changes to fixable results
 remediations = scan['remediation']['pin']
 for k, v in remediations.items():
+    update = False
     package, old_version = k.split('@')
     new_version = v['upgradeTo'].split('@')[1]
     print(package, old_version, new_version)
 
     # Check to see if package was explicitly included
+    # If it was not, then we only care about updating
+    # requirements.txt, not requirements.in
+    # 'make update-dependencies' will update requirements.txt
     with open('ckan/requirements.in', 'r') as source:
         all_requirements = source.readlines()
     if package in ', '.join(all_requirements):
         update = True
 
     if update:
-        # Remove old version
         os.system('sed -i "/^%s\\(=\\|>\\|$\\)/Id" ckan/requirements.in' % (package))
-        # Add new version if it was already being specified
         os.system("echo '%s' >> ckan/requirements.in" % (package + ">=" + new_version))
 
 # TODO: Handle unfixable results
