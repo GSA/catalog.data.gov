80c27756fe43f0396b6ceb521c2b679cede1bd55
jbrown-xentity <jbrown@xentity.com>
2023-04-05 15:21:19 -0600

    Add config appropriately

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,18 +1,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://ckan:5000',
-    specPattern: 'cypress/integration/*.cy.js'
-  },
+    e2e: {
+        baseUrl: 'http://ckan:5000',
+        specPattern: 'cypress/integration/*.cy.js'
+    },
 //   component: {
     
 //   }
-  videos: {
     videoCompression: false,
-    videoUploadOnPasses: false
-  },
-  screenshots: {
-    screenshotOnRunFailure: false
-  }
+    videoUploadOnPasses: false,
+    screenshotOnRunFailure: false,
 })
