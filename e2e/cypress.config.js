a97f294b1108b731e278450e4ddeec35136350b7
jbrown-xentity <jbrown@xentity.com>
2023-04-05 15:23:21 -0600

    comment cleanup

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -5,9 +5,6 @@ module.exports = defineConfig({
         baseUrl: 'http://ckan:5000',
         specPattern: 'cypress/integration/*.cy.js'
     },
-//   component: {
-    
-//   }
     videoCompression: false,
     videoUploadOnPasses: false,
     screenshotOnRunFailure: false,
