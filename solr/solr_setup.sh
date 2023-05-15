386824d0b63c9493fc769b8d66e7672c6796de61
Fuhu Xia <fxia@reisystems.com>
2023-05-15 15:40:21 -0400

    make sure it does not search any subfolder

--- solr/solr_setup.sh
+++ solr/solr_setup.sh
@@ -3,7 +3,7 @@
 mkdir -p /tmp/ckan_config
 
 # Restore last dated EFS backup if exists
-if [ "$(find /var/solr/data/*aws-backup-restore* 2>/dev/null)" ]; then
+if [ "$(find /var/solr/data/*aws-backup-restore* -maxdepth 0 2>/dev/null)" ]; then
   good_backup="$(find /var/solr/data/*aws-backup-restore* | tail -1)"
   echo "Found EFS backup. Restoring from $good_backup"
   rm -r /var/solr/data/ckan
