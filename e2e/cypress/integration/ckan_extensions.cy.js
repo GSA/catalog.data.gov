ae058f5ed07524d6cb4050f9d8d9155dec6935d8
Fuhu Xia <fxia@reisystems.com>
2023-07-21 10:54:17 -0400

    fix test

--- e2e/cypress/integration/ckan_extensions.cy.js
+++ e2e/cypress/integration/ckan_extensions.cy.js
@@ -1,5 +1,5 @@
 describe('CKAN Extensions', () => {
-    it('Uses CKAN 2.9', () => {
+    it('Uses CKAN 2.10', () => {
         cy.request('/api/action/status_show').should((response) => {
             expect(response.body).to.have.property('success', true);
             expect(response.body.result).to.have.property('ckan_version', '2.10.1');
