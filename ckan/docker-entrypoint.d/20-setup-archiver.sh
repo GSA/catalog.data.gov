b7ee0f79b64ee023dcd49b5037dfb1ed73ef92ce
Fuhu Xia <fxia@reisystems.com>
2025-03-04 18:38:50 -0500

    remove report metrics_dashboard

--- ckan/docker-entrypoint.d/20-setup-archiver.sh
+++
@@ -1,7 +0,0 @@
-# TODO: Re-enable pending https://github.com/GSA/data.gov/issues/3986
-
-# echo "Setup Archiver database table"
-# ckan archiver init
-
-echo "Setup Report database table"
-ckan db upgrade -p report
