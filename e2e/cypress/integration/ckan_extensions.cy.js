956cd1413d10e6dd2442eb3516f9d2b3e235b58e
Fuhu Xia <fxia@reisystems.com>
2025-03-10 14:40:43 -0400

    update some dependancies

--- e2e/cypress/integration/ckan_extensions.cy.js
+++ e2e/cypress/integration/ckan_extensions.cy.js
@@ -1,8 +1,8 @@
 describe('CKAN Extensions', () => {
-    it('Uses CKAN 2.10', () => {
+    it('Uses CKAN 2.11', () => {
         cy.request('/api/action/status_show').should((response) => {
             expect(response.body).to.have.property('success', true);
-            expect(response.body.result).to.have.property('ckan_version', '2.10.5');
+            expect(response.body.result).to.have.property('ckan_version', '2.11.0');
         });
     });
 
@@ -32,8 +32,6 @@ describe('CKAN Extensions', () => {
             expect(installed_extensions).to.include('dcat_json_interface');
             expect(installed_extensions).to.include('structured_data');
             expect(installed_extensions).to.include('datagovcatalog');
-            expect(installed_extensions).to.include('report');
-            expect(installed_extensions).to.include('metrics_dashboard');
             // TODO: Re-enable pending https://github.com/GSA/data.gov/issues/3986
             // expect(installed_extensions).to.include('archiver');
             // expect(installed_extensions).to.include('qa');
